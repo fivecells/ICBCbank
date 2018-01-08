82af4e18bf4edac9af3ed6612e34328189b35559
firstPro <biaoyuzhi@sina.com>
2018-01-08 20:58:44 +0800

    展示页面模糊查询和页面细微调整

---
+++ bank-manager/pojo/src/main/java/com/wenlong/pojo/vo/UserinfoSearch.java
@@ -0,0 +1,23 @@
+package com.wenlong.pojo.vo;
+
+public class UserinfoSearch {
+
+    private String userName;
+    private String userIdentity;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserIdentity() {
+        return userIdentity;
+    }
+
+    public void setUserIdentity(String userIdentity) {
+        this.userIdentity = userIdentity;
+    }
+}
